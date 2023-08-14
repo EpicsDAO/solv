970f2c35de0cdde6c0d28d199d8dd4d5723f1882
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-08-14 20:55:38 +0200

    update version

--- release.ts
+++ release.ts
@@ -1,6 +1,5 @@
 import { readFileSync, writeFileSync } from 'fs'
 import pjson from './package.json'
-import * as fsPromise from 'fs/promises'
 
 const CHANGE_LOG_PATH = './solv-debian/debian/changelog'
 const VERSION_PATH = './src/lib/version.ts'
@@ -20,9 +19,9 @@ const changeLogWrite = () => {
   const lines = file.split('\n')
   const firstLine = lines[0]
   const newFirstLine = firstLine.replace(/(\d+.\d+.\d+)/, pjson.version)
-  const newLastLine = ` -- EpicsDAO <info@epics.dev>  ${new Date().toUTCString()}`
+  // const newLastLine = ` -- EpicsDAO <info@epics.dev>  ${new Date().toUTCString()}`
   lines[0] = newFirstLine
-  lines[lines.length - 1] = newLastLine
+  // lines[lines.length - 1] = newLastLine
   const newFile = lines.join('\n')
   writeFileSync(CHANGE_LOG_PATH, newFile, {
     flag: 'w',
