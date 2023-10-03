fd4a2c1e70ab709352bc47e2767ff6a7218798c4
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-10-03 19:22:52 +0200

    update solv update cmd

--- src/cli/update/index.ts
+++ src/cli/update/index.ts
@@ -16,7 +16,7 @@ export const updateCommands = async () => {
     )
     .option(
       '-m, --maxDelinquentStake <maxDelinquentStake>',
-      'Max Delinquent Stake e.g 5',
+      `Max Delinquent Stake e.g ${SolvConfig.DEFAULT_DELINQUENT_STAKE}`,
       `${SolvConfig.DEFAULT_DELINQUENT_STAKE}`
     )
     .option(
