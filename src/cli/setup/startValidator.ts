2b6aa3cc44010a0fc6143b47518b2ea898a6f5f3
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-10-20 20:03:32 +0200

    update: release v1.5.5

--- src/cli/setup/startValidator.ts
+++ src/cli/setup/startValidator.ts
@@ -1,5 +1,6 @@
 import { SOLV_ROOT, VALIDATOR_STARTUP_SCRIPT } from '@/config'
 import { startValidatorSh } from '@/template/startValitatorSh'
+import { spawnSync } from 'child_process'
 import { existsSync, mkdirSync, writeFileSync } from 'fs'
 
 export const startValidator = (fetchSnapshot = false) => {
@@ -9,6 +10,8 @@ export const startValidator = (fetchSnapshot = false) => {
       mkdirSync(SOLV_ROOT, { recursive: true })
     }
     writeFileSync(`${VALIDATOR_STARTUP_SCRIPT}`, body, 'utf-8')
+    const cmd = `sudo chmod +x ${VALIDATOR_STARTUP_SCRIPT}`
+    spawnSync(cmd, { shell: true, stdio: 'inherit' })
   } catch (error) {
     throw new Error(`startValidator Error: ${error}`)
   }
