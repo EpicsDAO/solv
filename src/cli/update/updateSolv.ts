e92a8ebb2fa394e88431072c48b70f0881d2945d
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-10-24 16:38:51 +0200

    update: release v1.5.9

--- src/cli/update/updateSolv.ts
+++ src/cli/update/updateSolv.ts
@@ -1,8 +1,6 @@
 import { spawnSync } from 'child_process'
 
 export const updateSolv = () => {
-  const cmd = [`sudo apt update`]
-  spawnSync(cmd.join(' && '), { shell: true, stdio: 'inherit' })
-  const cmd2 = [`sudo apt -y install solv`]
-  spawnSync(cmd2.join(' && '), { shell: true, stdio: 'inherit' })
+  const cmd = `npm i -g @epics-dao/solv`
+  spawnSync(cmd, { shell: true, stdio: 'inherit' })
 }
