79390a26af046a127b76a5ebf1c6bc480da44c85
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-03 18:05:42 +0100

    update: release v1.6.0

--- src/cli/restart/index.ts
+++ src/cli/restart/index.ts
@@ -1,8 +1,8 @@
-import { VALIDATOR_STARTUP_SCRIPT } from '@/config'
+import { SNAPSHOT_PATH, VALIDATOR_STARTUP_SCRIPT } from '@/config'
 import { program } from '@/index'
 import { startValidatorSh } from '@/template/startValitatorSh'
 import { spawnSync } from 'child_process'
-import { chmodSync, writeFileSync } from 'fs'
+import { chmodSync, rmSync, writeFileSync } from 'fs'
 
 export const restartCommand = () => {
   program
@@ -11,6 +11,8 @@ export const restartCommand = () => {
     .option('--snapshot', 'Restart Solana Validator with fetch snapshot', false)
     .action(async (options) => {
       if (options.snapshot) {
+        const cmd = `sudo rm -rf ${SNAPSHOT_PATH}`
+        spawnSync(cmd, { shell: true, stdio: 'inherit' })
         const startValidator = startValidatorSh(true)
         writeFileSync(VALIDATOR_STARTUP_SCRIPT, startValidator)
         chmodSync(VALIDATOR_STARTUP_SCRIPT, '755')
