5db9a11f112a70c4ee28a4e0f92d682e7c984816
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-01-06 18:14:55 +0100

    update solana v1.16.25

--- src/cli/restart/index.ts
+++ src/cli/restart/index.ts
@@ -1,4 +1,4 @@
-import { SNAPSHOT_PATH, VALIDATOR_STARTUP_SCRIPT } from '@/config'
+import { VALIDATOR_STARTUP_SCRIPT } from '@/config'
 import { program } from '@/index'
 import { startValidatorSh } from '@/template/startValitatorSh'
 import { spawnSync } from 'child_process'
@@ -11,8 +11,10 @@ export const restartCommand = () => {
     .option('--snapshot', 'Restart Solana Validator with fetch snapshot', false)
     .action(async (options) => {
       if (options.snapshot) {
-        const cmd = `sudo rm -rf ${SNAPSHOT_PATH}`
+        const cmd = `sudo rm -rf /mt/ledger/validator-ledger/snapshot*`
         spawnSync(cmd, { shell: true, stdio: 'inherit' })
+        const cmd2 = `sudo rm -rf /mt/ledger/validator-ledger/rocksdb`
+        spawnSync(cmd2, { shell: true, stdio: 'inherit' })
         const startValidator = startValidatorSh(true)
         writeFileSync(VALIDATOR_STARTUP_SCRIPT, startValidator)
         chmodSync(VALIDATOR_STARTUP_SCRIPT, '755')
