dc856ca3f50adce1c31171abd47ea15b7405cb43
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-01-18 11:06:29 +0100

    update disks

--- src/cli/check/mt/getLargestDisk.ts
+++ src/cli/check/mt/getLargestDisk.ts
@@ -6,7 +6,7 @@ type DiskInfo = {
   mountpoint: string
 }
 
-function getPreferredDisks(excludeNames: string[] = ['sda']): DiskInfo[] {
+function getPreferredDisks(): DiskInfo[] {
   const commandOutput = execSync('lsblk -l -b -o NAME,SIZE,MOUNTPOINT', {
     encoding: 'utf8',
   })
@@ -16,24 +16,26 @@ function getPreferredDisks(excludeNames: string[] = ['sda']): DiskInfo[] {
 
   for (const line of lines) {
     const [name, sizeStr, mountpoint] = line.trim().split(/\s+/)
-    if (!name || !sizeStr || excludeNames.includes(name)) continue; // Skip empty lines, lines without required data, or excluded disks
+    if (!name || !sizeStr) continue; // Skip empty lines or lines without required data
 
     const size = parseInt(sizeStr, 10)
     if (isNaN(size)) continue; // Skip lines where size is not a number
 
     if (size >= 490 * 1024 * 1024 * 1024) {
-      // check if disk size is 490GB or more
       const diskInfo: DiskInfo = { name, size, mountpoint: mountpoint || '' }
       disks.push(diskInfo)
     }
   }
 
-  // Sort disks first by mountpoint (unmounted first) and then by size (larger first)
+  // Sort disks first by mountpoint (unmounted first), then by whether the name ends with a number, and then by size (larger first)
   return disks.sort((a, b) => {
     if (a.mountpoint === '' && b.mountpoint !== '') return -1;
     if (a.mountpoint !== '' && b.mountpoint === '') return 1;
+    if (/\d$/.test(a.name) && !/\d$/.test(b.name)) return 1;
+    if (!/\d$/.test(a.name) && /\d$/.test(b.name)) return -1;
     return b.size - a.size;
   })
 }
+
 
 export default getPreferredDisks
