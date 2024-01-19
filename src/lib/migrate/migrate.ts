c00136f384aaed0e64c049425d0a3f432a7ad6f8
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-01-19 19:51:05 +0100

    add get disk

--- src/lib/migrate/migrate.ts
+++ src/lib/migrate/migrate.ts
@@ -9,26 +9,26 @@ import { rmSwap } from './rmSwap'
 import inquirer from 'inquirer'
 
 export const migrate = async () => {
-  const confirm = await inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'confirm',
-      message: 'Are you sure you want to migrate solv3?',
-      default() {
-        return false
-      },
-    },
-  ])
-  if (!confirm.confirm) {
-    return false
-  }
-  console.log('Stopping solv service...')
-  spawnSync('sudo systemctl stop solv', { shell: true, stdio: 'inherit' })
-  await sleep(7000)
-  mvKeys()
-  rmSwap()
-  removeFstabEntries()
-  rmServices()
-  rmMtDir()
+  // const confirm = await inquirer.prompt([
+  //   {
+  //     type: 'confirm',
+  //     name: 'confirm',
+  //     message: 'Are you sure you want to migrate solv3?',
+  //     default() {
+  //       return false
+  //     },
+  //   },
+  // ])
+  // if (!confirm.confirm) {
+  //   return false
+  // }
+  // console.log('Stopping solv service...')
+  // spawnSync('sudo systemctl stop solv', { shell: true, stdio: 'inherit' })
+  // await sleep(7000)
+  // mvKeys()
+  // rmSwap()
+  // removeFstabEntries()
+  // rmServices()
+  // rmMtDir()
   migrateSetup()
 }
