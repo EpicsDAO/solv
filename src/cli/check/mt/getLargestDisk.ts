738f62f4c84c38e591f1bb9efcf7099aae91dcd3
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-01-19 19:19:57 +0100

    add get disk

--- src/cli/check/mt/getLargestDisk.ts
+++ src/cli/check/mt/getLargestDisk.ts
@@ -20,11 +20,6 @@ function getPreferredDisks(): GetPreferredDisksResult {
   const lines = commandOutput.split('\n').slice(1) // skip the header line
 
   const disks: DiskInfo[] = []
-
-  // Collecting all disk names to identify which ones have partitions
-  const allDiskNames = lines.map((line) => line.trim().split(/\s+/)[0])
-
-  // Initialize the boolean flags
   let has850GB = false
   let has400GB = false
   let hasUsed1250GB = false
@@ -41,39 +36,14 @@ function getPreferredDisks(): GetPreferredDisksResult {
       disks.push(diskInfo)
 
       // Check conditions
-      const hasPartition = allDiskNames.some(
-        (diskName) => diskName !== name && diskName.startsWith(name),
-      )
-      if (size >= 850 * 1024 * 1024 * 1024 && !hasPartition) has850GB = true
-      if (size >= 400 * 1024 * 1024 * 1024 && !hasPartition) has400GB = true
+      if (size >= 850 * 1024 * 1024 * 1024 && mountpoint) has850GB = true
+      if (size >= 400 * 1024 * 1024 * 1024 && mountpoint) has400GB = true
       if (size >= 1250 * 1024 * 1024 * 1024 && mountpoint) hasUsed1250GB = true
     }
   }
 
-  // Custom sort function
-  const sortDisks = (a: DiskInfo, b: DiskInfo) => {
-    // Check for mountpoint
-    if (a.mountpoint === '' && b.mountpoint !== '') return -1
-    if (a.mountpoint !== '' && b.mountpoint === '') return 1
-
-    // Check if disk has partitions
-    const hasPartitionA = allDiskNames.some(
-      (diskName) => diskName !== a.name && diskName.startsWith(a.name),
-    )
-    const hasPartitionB = allDiskNames.some(
-      (diskName) => diskName !== b.name && diskName.startsWith(b.name),
-    )
-
-    // Sort by whether the disk has partitions
-    if (hasPartitionA && !hasPartitionB) return 1
-    if (!hasPartitionA && hasPartitionB) return -1
-
-    // Finally, sort by size
-    return b.size - a.size
-  }
-
-  // Sort disks
-  const sortedDisks = disks.sort(sortDisks)
+  // Sort disks by size
+  const sortedDisks = disks.sort((a, b) => b.size - a.size)
 
   return { disks: sortedDisks, has850GB, has400GB, hasUsed1250GB }
 }
