3c0c5d230fcc9a15335c759f761ea2211efa4b56
Fumiya Yamanaka <f.yamanaka1127@gmail.com>
2024-02-11 20:08:06 +0900

    Handle json parse error

--- src/lib/readOrCreateDefaultConfig.ts
+++ src/lib/readOrCreateDefaultConfig.ts
@@ -12,16 +12,18 @@ export type ConfigParams = {
 export const readOrCreateDefaultConfig = () => {
   const homeDir = os.homedir()
   const configPath = `${homeDir}/${FILES.CONFIG}`
-  if (existsSync(configPath)) {
-    const config = JSON.parse(readFileSync(configPath, 'utf-8')) as CONFIG_TYPE
-    let locale = readLocale(config.LANG)
-    return {
-      config,
-      locale,
-    } as ConfigParams
+  if (!existsSync(configPath)) {
+    writeFileSync(configPath, JSON.stringify(CONFIG, null, 2))
+    console.log(`Created default config file at ${configPath}`)
   }
-  writeFileSync(configPath, JSON.stringify(CONFIG, null, 2))
-  console.log(`Created default config file at ${configPath}`)
-  let locale = readLocale(CONFIG.LANG)
-  return { config: CONFIG, locale } as ConfigParams
+  let config: CONFIG_TYPE
+  try {
+    config = JSON.parse(readFileSync(configPath, 'utf-8')) as CONFIG_TYPE
+  } catch (error) {
+    console.log('Error parsing config file. Renewing config file.')
+    writeFileSync(configPath, JSON.stringify(CONFIG, null, 2))
+    config = CONFIG
+  }
+  let locale = readLocale(config.LANG)
+  return { config, locale } as ConfigParams
 }
