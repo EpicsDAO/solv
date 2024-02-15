80fa77c1ff563cd005315798fd27ec87b4e69201
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-02-15 13:35:19 +0100

    update jito version to v1.17.22-jito

--- src/config/jitConfig.ts
+++ src/config/jitConfig.ts
@@ -8,8 +8,8 @@ export interface JitoConfig {
 }
 
 export const JITO_CONFIG: JitoConfig = {
-  version: '1.17.20',
-  tag: 'v1.17.20-jito',
+  version: '1.17.22',
+  tag: 'v1.17.22-jito',
   commissionBps: 700,
   relayerUrl: 'http://amsterdam.mainnet.relayer.jito.wtf:8100',
   blockEngineUrl: 'https://amsterdam.mainnet.block-engine.jito.wtf',
