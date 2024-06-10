37f75bfeb94897010e1f21bd063d8bd3a4beefcc
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-06-10 17:25:13 +0200

    update - solv i for jito client

--- packages/solv/src/cli/install/index.ts
+++ packages/solv/src/cli/install/index.ts
@@ -1,7 +1,8 @@
 import { program } from '@/index'
 import { updateVersion } from '../update'
-import { CONFIG } from '@/config/config'
+import { CONFIG, MAINNET_TYPES } from '@/config/config'
 import { ConfigParams } from '@/lib/readOrCreateDefaultConfig'
+import { jitoUpdate } from '../update/jitoUpdate'
 
 export const installCommands = (solvConfig: ConfigParams) => {
   const { cmds } = solvConfig.locale
@@ -15,6 +16,11 @@ export const installCommands = (solvConfig: ConfigParams) => {
       CONFIG.SOLANA_VERSION,
     )
     .action((options: { version: string }) => {
+      if (solvConfig.config.MAINNET_TYPE === MAINNET_TYPES.JITO_MEV) {
+        const jitoTag = `v${options.version}-jito`
+        jitoUpdate(jitoTag)
+        return
+      }
       updateVersion(options.version)
     })
 }
--- packages/solv/src/cli/update/jitoUpdate.ts
+++ packages/solv/src/cli/update/jitoUpdate.ts
@@ -1,9 +1,8 @@
 import { JITO_CONFIG } from '@/config/jitConfig'
 import { spawnSync } from 'child_process'
 
-export const jitoUpdate = async () => {
-  const TAG = `${JITO_CONFIG.tag}`
-  spawnSync(`sh -c "$(curl -sSfL https://release.jito.wtf/${TAG}/install)"`, {
+export const jitoUpdate = (tag = JITO_CONFIG.tag) => {
+  spawnSync(`sh -c "$(curl -sSfL https://release.jito.wtf/${tag}/install)"`, {
     shell: true,
     stdio: 'inherit',
   })
