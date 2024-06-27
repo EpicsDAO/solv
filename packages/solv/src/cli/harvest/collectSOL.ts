5441abedeefd684d42f72cf12fb2ee47912ffc3c
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-06-27 18:30:47 +0200

    update cron/harvest

--- packages/solv/src/cli/harvest/collectSOL.ts
+++ packages/solv/src/cli/harvest/collectSOL.ts
@@ -4,6 +4,7 @@ import { execSync, spawnSync } from 'child_process'
 import { homedir } from 'os'
 import { getHarvestBalance } from './getHarvestBalance'
 import getBalance, { KeyType } from '@/lib/solana/getBalance'
+import chalk from 'chalk'
 
 export const collectSOL = () => {
   const homeDir = homedir()
@@ -16,20 +17,29 @@ export const collectSOL = () => {
   const voteAccountBalance = getBalance(KeyType.VOTE)
 
   if (voteAccountBalance < 1) {
-    console.log('Vote Account Balance is less than 1 SOL')
-    return
+    console.log(chalk.white('Vote Account Balance is less than 1 SOL'))
+    console.log(
+      chalk.white(
+        'Skip withdrawing SOL from Vote Account to Authority Account',
+      ),
+    )
+  } else {
+    // Withdraw all SOL from Vote Account to Authority Account
+    const cmd = `solv withdraw --all`
+    spawnSync(cmd, { shell: true, stdio: 'inherit' })
   }
-
-  // Withdraw all SOL from Vote Account to Authority Account
-  const cmd = `solv withdraw --all`
-  spawnSync(cmd, { shell: true, stdio: 'inherit' })
 
   // Check Validator Key Balance
   const validatorTransferableBalance = getHarvestBalance()
 
   // Transfer SOL from Validator Account to Authority Account
+  const toAddress = execSync(
+    `solana address --keypair ${mainnetValidatorAuthorityKey}`,
+  )
+    .toString()
+    .trim()
   const result = spawnSync(
-    `solana transfer ${mainnetValidatorAuthorityKey} ${validatorTransferableBalance} --url ${SOLANA_RPC_URL} --keypair ${mainnetValidatorKey}`,
+    `solana transfer ${toAddress} ${validatorTransferableBalance} --url ${SOLANA_RPC_URL} --keypair ${mainnetValidatorKey}`,
     { shell: true },
   )
   if (result.status !== 0) {
