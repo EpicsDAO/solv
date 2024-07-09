08f0f095ac2a791e293fbf24cb8285ff8942f835
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-07-09 18:47:37 +0200

    update - cron

--- packages/solv/src/cli/cron/hasEpochTimer.ts
+++ packages/solv/src/cli/cron/hasEpochTimer.ts
@@ -3,6 +3,7 @@ import { readFile } from 'fs/promises'
 const hasEpochTimer = async () => {
   try {
     const crontabList = await readFile('/etc/crontab', 'utf-8')
+    console.log(crontabList)
     return crontabList.includes('solv epochTimer')
   } catch (error) {
     return false
--- packages/solv/src/cli/cron/removeCronJob.ts
+++ packages/solv/src/cli/cron/removeCronJob.ts
@@ -2,7 +2,7 @@ import { spawnSync } from 'child_process'
 import chalk from 'chalk'
 import { promises as fs } from 'fs'
 
-const removeCronJob = async (cronPattern = '*/5 * * * *'): Promise<void> => {
+const removeCronJob = async (cronPattern = 'epochTimer'): Promise<void> => {
   try {
     // 現在のcrontabの内容を取得
     const crontabResult = spawnSync('crontab', ['-l'], { encoding: 'utf-8' })
@@ -13,19 +13,14 @@ const removeCronJob = async (cronPattern = '*/5 * * * *'): Promise<void> => {
 
     const crontab = crontabResult.stdout.split('\n')
 
-    // 削除するcronジョブのパターン
-    const cronJobPattern = new RegExp(
-      `^${cronPattern.replace(/\*/g, '\\*')} .*solv epochTimer.*$`,
-    )
-
-    // 指定された行を削除
-    const updatedCrontab = crontab
-      .filter((line) => !cronJobPattern.test(line))
+    // 指定したcronPatternを含まない行だけを抽出
+    const newCronBody = crontab
+      .filter((line) => !line.includes(cronPattern))
       .join('\n')
 
     // 新しいcrontabの内容を設定
     const tempCrontabFile = '/tmp/temp_crontab'
-    await fs.writeFile(tempCrontabFile, updatedCrontab, 'utf-8')
+    await fs.writeFile(tempCrontabFile, newCronBody, 'utf-8')
     spawnSync('crontab', [tempCrontabFile])
     await fs.unlink(tempCrontabFile) // 一時ファイルを削除
 
