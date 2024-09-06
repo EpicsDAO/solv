e344f12fb223dc7ba2a7c1d6551ef8f952f5f3c4
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-09-06 10:16:04 +0200

    testing log

--- packages/solv/src/cli/log/tail.ts
+++ packages/solv/src/cli/log/tail.ts
@@ -1,5 +1,5 @@
 import { startupScriptPaths } from '@/config/config'
-import { spawn } from 'node:child_process'
+import { spawnSync } from 'node:child_process'
 
 export type TailOptions = {
   info: boolean
@@ -21,34 +21,19 @@ export const tail = (options: TailOptions) => {
     }
 
     console.log(cmd)
-    const child = spawn(cmd, { shell: true, stdio: 'inherit' })
-    const childPid = child.pid
-    if (!childPid) {
-      throw new Error('Failed to start tail process')
+    const result = spawnSync(cmd, { shell: true, stdio: 'inherit' })
+
+    if (result.error) {
+      throw result.error
     }
 
-    // Handle the SIGINT signal (Ctrl+C)
-    process.on('SIGINT', () => {
-      console.log('Caught interrupt signal, stopping tail...')
-      process.kill(childPid, 'SIGINT') // Send SIGINT to child process
+    if (result.signal === 'SIGINT') {
+      console.log('Child process terminated due to receipt of SIGINT signal')
       process.exit(0)
-    })
-
-    child.on('exit', (code, signal) => {
-      if (signal === 'SIGINT') {
-        console.log('Child process terminated due to receipt of SIGINT signal')
-        process.exit(0)
-      } else if (code !== 0) {
-        console.error(`Child process exited with code ${code}`)
-        process.exit(code)
-      }
-    })
-
-    child.on('error', (error) => {
-      console.error(`tail Error: ${error}`)
-      process.kill(childPid, 'SIGINT') // Ensure child process is terminated on error
-      process.exit(1)
-    })
+    } else if (result.status !== 0) {
+      console.error(`Child process exited with code ${result.status}`)
+      process.exit(result.status)
+    }
   } catch (error) {
     console.error(`tail Error: ${error}`)
     process.exit(1)
