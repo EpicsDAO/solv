e2047519b597d2b390d8713f567a1c6eadcd3194
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-09-13 09:00:26 +0200

    Update Solana Version for Testnet v2.0.9

---
+++ .changeset/silly-ears-taste.md
@@ -0,0 +1,27 @@
+---
+'@epics-dao/solv': patch
+---
+
+Update Solana Version for Testnet v2.0.9
+
+```bash
+solv update
+```
+
+```bash
+solv update --config
+```
+
+```bash
+solv restart
+```
+
+or
+
+```bash
+solv update && solv update -b
+```
+
+Validators DAO:
+
+https://discord.gg/C7ZQSrCkYR
--- packages/solv/src/config/versionConfig.ts
+++ packages/solv/src/config/versionConfig.ts
@@ -1,9 +1,9 @@
-export const VERSION_TESTNET = '2.0.8'
+export const VERSION_TESTNET = '2.0.9'
 export const VERSION_MAINNET = '1.18.23'
-export const VERSION_JITO_TESTNET = '2.0.8'
+export const VERSION_JITO_TESTNET = '2.0.9'
 export const VERSION_JITO_MAINNET = '1.18.23'
-export const VERSION_JITO_RPC = '2.0.8'
-export const VERSION_SOLANA_RPC = '2.0.8'
+export const VERSION_JITO_RPC = '2.0.9'
+export const VERSION_SOLANA_RPC = '2.0.9'
 export const DELINQUENT_STAKE_TESTNET = 5
 export const DELINQUENT_STAKE_MAINNET = 5
 export const COMMISSION = 5
